1211ab4d5239eef9756c010f69b3b5e49402bdc4
TomohiroSakaguchi <stomohiro59@gmail.com>
2019-10-01 19:24:56 +0900

    とりあえず残響分離ができた

--- Dereverberation.m
+++ Dereverberation.m
@@ -9,7 +9,7 @@ T = strcat(PathName, FileName);
 
 signal = signal(1:30*Fs,1);%特定の部分だけ抜き出し
 len_data = length(signal);
-N = 300; %単位:ms
+%%
 windowLen = 2^8;         % window length
 shiftLen  = windowLen/8; % shifting stepsize
 fftLen    = windowLen;   % number of FFT points
@@ -17,13 +17,14 @@ rotateFlag    = true; % deciding phase convention of DGT
 zeroPhaseFlag = true; % deciding phase convention of window
 %% 後期残響の予測の準備
 tau = 70;%70~120msの範囲で調整(初期残響時間分)
+N = tau/2;
 tau_sample = fix((tau/1000)*Fs);
 N_sample = fix((N/1000)*Fs);
 lp_range = tau_sample:(len_data-1);
 %% 線形予測
 %data_w = prewhiten(signal(:,1));%事前白色化
 pcc = lpc(signal(lp_range,1),N_sample);%後期残響予測フィルターの計算
-LRP = 0.7.*(filter(pcc,1,signal(:,1)));%元データに対して後期残響フィルターかけを行い，後期残響を導出
+LRP = 0.7.*(filter([0 -pcc(2:end)],1,signal(:,1)));%元データに対して後期残響フィルターかけを行い，後期残響を導出
 %% DGTの準備
 [win,diffWin] = generalizedCosWin(windowLen,'nuttall4termC1'); %窓関数の生成
 signal = zeroPaddingForDGT(signal,shiftLen,fftLen); % DGTを実行させる前にゼロ埋め
@@ -72,6 +73,8 @@ end
 %% 位相修正
 IF = calcInstFreq(S_hat,diffSpec,fftLen,windowLen,rotateFlag);% 瞬時周波数を計算する関数，源信号のDGT，微分窓をDGTしたものを使う
 iPCspec = instPhaseCorrection(S_hat,IF,shiftLen,fftLen);% 瞬時位相修正済スペクトログラムを計算する関数
+IFrev = calcInstFreq(LRPspec,diffSpec,fftLen,windowLen,rotateFlag);
+iPCrev = instPhaseCorrection(LRPspec,IFrev,shiftLen,fftLen);
 
 figure, cRange = max(abs(spec(:)))/5;
 subplot(1,3,1), imagesc(abs(spec)), axis xy, caxis(cRange*[-1 1])
@@ -80,9 +83,10 @@ subplot(1,3,2), imagesc(real(spec)), axis xy, caxis(cRange*[-1 1])
 title 'Real part'
 subplot(1,3,3), imagesc(real(iPCspec)), axis xy, caxis(cRange*[-1 1])
 title 'Real part (iPC)'
-spec = invInstPhaseCorrection(iPCspec,IF,shiftLen,fftLen); % 位相逆回転
+recon_spec = invInstPhaseCorrection(iPCspec,IF,shiftLen,fftLen); % 位相逆回転
+recon_rev = invInstPhaseCorrection(iPCrev,IF,shiftLen,fftLen);
 figure, imagesc(20*log10(abs(spec))), colorbar, axis xy
 %% 直接音再構成
 dualWin = calcCanonicalDualWindow(win,shiftLen);% 
-reconst = invDGT(spec,dualWin,shiftLen,fftLen,rotateFlag,zeroPhaseFlag);% 位相修正逆変換したものを逆DGTし
-
+reconst = invDGT(recon_spec,dualWin,shiftLen,fftLen,rotateFlag,zeroPhaseFlag);% 位相修正逆変換したものを逆DGTし
+reconstrev = invDGT(recon_rev,dualWin,shiftLen,fftLen,rotateFlag,zeroPhaseFlag);
